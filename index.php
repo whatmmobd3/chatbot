528c1a37fc44957c93fecf429f2780579ed26e0c
David <>
2021-03-23 11:38:30 +0700

    update

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
-    class API {
-        function Select() {
-            $users = '[{ "name":"PHP", "desc":"chatbot" }]';
-            return json_decode($users);
+namespace Zalo;
+
+use Zalo\Builder\MessageBuilder;
+use Zalo\ZaloEndPoint;
+
+class ZaloSdkTest{
+    function API($accessToken) {
+        $msgBuilder = new MessageBuilder('text');
+        $msgBuilder->withUserId('494021888309207992');
+        $msgBuilder->withText('Message Text');
+        $msgText = $msgBuilder->build();
+
+        // goi api
+        $response = $this->zalo->post(ZaloEndPoint::API_OA_SEND_MESSAGE, $accessToken, $msgText);
+        echo '<br><br>';
+        // lay du lieu tra ve
+        print_r($response->getDecodedBody());
+        echo '<br><br>';
+        // return $response->getDecodedBody();
         }
     }
     $API = new API;
     header('Content-Type: application/json');
-    echo $API ->Select()
+    echo $API ->API('EA2Aus71pe4wUxIJP4Sv')
+
 ?>
